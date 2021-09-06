acdcd5c7ef8f3b75cd6f50dc4fd55afc25b7e526
YassineElGhizi <86019502+YassineElGhizi@users.noreply.github.com>
2021-09-06 16:05:30 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from flask_cors import CORS , cross_origin
+from test import google as g
+from flask import request
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/search', methods=['POST'])
+@cross_origin()
+def predict():
+    if request.method == 'POST':
+        print("request cames from : " +str(request.environ['REMOTE_ADDR']))
+        res = dict()
+        req = request.get_json()
+        topic = req["topic"]
+        res["response"] = g(topic)
+        print("res last state ")
+        print(res)
+        return res
+    else:
+        return "ERR Flask"
+
+if __name__ == '__main__':
+    app.run(debug=True)
